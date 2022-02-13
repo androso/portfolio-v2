b9e089659c948befcf4feba33a4b571bd78ffe4f
Anibal <androso421@gmail.com>
2022-02-13 15:18:08 -0600

    adding open sans

--- styles/fonts.js
+++ styles/fonts.js
@@ -28,6 +28,26 @@ const Fonts = css`
     font-weight: 900;
     font-style: normal;
   }
+  @font-face {
+    font-family: 'Open Sans';
+    src: url('../fonts/OpenSans/OpenSans-Regular.ttf') format('ttf');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Open Sans';
+    src: url('../fonts/OpenSans/OpenSans-Medium.ttf') format('ttf');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Open Sans';
+    src: url('../fonts/OpenSans/OpenSans-Bold.ttf') format('ttf');
+    font-weight: 700;
+    font-style: normal;
+  }
 `;
 
 export default Fonts;
